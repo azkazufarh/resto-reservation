954a904353c5f1bf87ebec8ce491b3750afb729c
hanifzzqr <azkazhanif@gmail.com>
2022-12-19 16:13:11 +0700

    second commit

---
+++ pages/user/menu.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Grid Food Menu</title>
+    <!--Google Font-->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <!--Stylesheet-->
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+    <div class="menu">
+        <div class="heading">
+            <h1>Goks Resto</h1>
+            <h3>&mdash; MENU &mdash; </h3>
+        </div>
+        <div class="food-items">
+            <img src="borger.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Borger Ayam</h5>
+                    <h5 class="price"> Rp 15.000 </h5>
+                </div>
+                <p>Burger daging sapi dengan saus nikmat dan roti.</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="food-items">
+            <img src="chicken.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Ayam Bakar Goks</h5>
+                    <h5 class="price"> Rp 25.000 </h5>
+                </div>
+                <p>Ayam dibakar pake madu manis.</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="food-items">
+            <img src="sotoayam.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Soto Ayam</h5>
+                    <h5 class="price"> Rp 20.000 </h5>
+                </div>
+                <p>Makanan Tradisional Indonesia.</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="food-items">
+            <img src="Spaghetti.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Spaghetti</h5>
+                    <h5 class="price"> Rp 30.000 </h5>
+                </div>
+                <p>Spaghetti pake Bakso authentic</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="food-items">
+            <img src="salmon.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Salmon Panggang</h5>
+                    <h5 class="price"> Rp 40.000 </h5>
+                </div>
+                <p>Salmon di Panggang</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="food-items">
+            <img src="Sandwich.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Sandwich</h5>
+                    <h5 class="price"> Rp 10.000 </h5>
+                </div>
+                <p>Roti dengan ayam, keju dan selada.</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
